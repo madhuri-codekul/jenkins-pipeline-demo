b92b7b9512329ba2a459155e62b5c0ecf2ffe2a3
Madhuri Tayade <melayer.madhuri@gmail.com>
2021-10-06 11:22:44 +0530

    Initial commit

---
+++ src/test/java/com/codekul/dockerimage/DockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codekul.dockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
